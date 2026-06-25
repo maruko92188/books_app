25ece93ec7468a5effdca9098cf72ba7fd20a677
maruko92188 <maruko.kazuki.f@gmail.com>
2026-06-26 02:26:10 +0900

    Userモデルに属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_19_163342) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_25_171402) do
   create_table "books", force: :cascade do |t|
     t.string "author"
     t.datetime "created_at", null: false
@@ -21,12 +21,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_19_163342) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "address"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "post_code"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.text "self_introduction"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
